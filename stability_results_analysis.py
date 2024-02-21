494e97e47fbbba0ce7812304b3d93b7d256f1fdc
HUIYI WANG <huiyi.wang@mail.mcgill.ca>
2024-02-21 15:09:25 -0500

    modified the stability_results_analysis.py

--- stability_results_analysis.py
+++ stability_results_analysis.py
@@ -29,30 +29,7 @@ def storeData(env, model, steps, env_name, file_name):
                                             'pelvis', 'root', 'talus_l', 'talus_r', 'tibia_l', 'tibia_r', 'toes_l', 'toes_r', 'world']
     joint_names = ['ankle_angle_l', 'ankle_angle_r', 'hip_adduction_l', 'hip_adduction_r', 'hip_flexion_l', 
                                             'hip_flexion_r', 'hip_rotation_l', 'hip_rotation_r', 'knee_angle_l', 'knee_angle_r',  
-                                            'hip_flexion_r', 'hip_rotation_l', 'hip_rotation_r', 'knee_angle_l', 'knee_angle_r',  
                                             'mtp_angle_l', 'mtp_angle_r', 'subtalar_angle_l', 'subtalar_angle_r']
-    '''
-    tendon_names = ['addbrev_l_tendon', 'addbrev_r_tendon', 'addlong_l_tendon', 'addlong_r_tendon', 'addmagDist_l_tendon', 
-                    'addmagDist_r_tendon', 'addmagIsch_l_tendon', 'addmagIsch_r_tendon', 'addmagMid_l_tendon', 'addmagMid_r_tendon', 
-                    'addmagProx_l_tendon', 'addmagProx_r_tendon', 'bflh_l_tendon', 'bflh_r_tendon', 'bfsh_l_tendon', 'bfsh_r_tendon', 
-                    'edl_l_tendon', 'edl_r_tendon', 'ehl_l_tendon', 'ehl_r_tendon', 'fdl_l_tendon', 'fdl_r_tendon', 'fhl_l_tendon', 
-                    'fhl_r_tendon', 'gaslat_l_tendon', 'gaslat_r_tendon', 'gasmed_l_tendon', 'gasmed_r_tendon', 'glmax1_l_tendon', 
-                    'glmax1_r_tendon', 'glmax2_l_tendon', 'glmax2_r_tendon', 'glmax3_l_tendon', 'glmax3_r_tendon', 'glmed1_l_tendon', 
-                    'glmed1_r_tendon', 'glmed2_l_tendon', 'glmed2_r_tendon', 'glmed3_l_tendon', 'glmed3_r_tendon', 'glmin1_l_tendon', 
-                    'glmin1_r_tendon', 'glmin2_l_tendon', 'glmin2_r_tendon', 'glmin3_l_tendon', 'glmin3_r_tendon', 'grac_l_tendon', 
-                    'grac_r_tendon', 'iliacus_l_tendon', 'iliacus_r_tendon', 'perbrev_l_tendon', 'perbrev_r_tendon', 'perlong_l_tendon', 
-                    'perlong_r_tendon', 'piri_l_tendon', 'piri_r_tendon', 'psoas_l_tendon', 'psoas_r_tendon', 'recfem_l_tendon', 'recfem_r_tendon', 'sart_l_tendon', 'sart_r_tendon', 'semimem_l_tendon', 'semimem_r_tendon', 'semiten_l_tendon', 'semiten_r_tendon', 'soleus_l_tendon', 'soleus_r_tendon', 'tfl_l_tendon', 'tfl_r_tendon', 'tibant_l_tendon', 'tibant_r_tendon', 'tibpost_l_tendon', 'tibpost_r_tendon', 'vasint_l_tendon', 'vasint_r_tendon', 'vaslat_l_tendon', 'vaslat_r_tendon', 'vasmed_l_tendon', 'vasmed_r_tendon']
-    '''
-    actuator_names =  ['addbrev_l', 'addbrev_r', 'addlong_l', 'addlong_r', 'addmagDist_l', 'addmagDist_r', 'addmagIsch_l', 'addmagIsch_r', 
-                       'addmagMid_l', 'addmagMid_r', 'addmagProx_l', 'addmagProx_r', 'glmax1_l', 'glmax1_r', 'glmax2_l', 'glmax2_r', 'glmax3_l', 
-                       'glmax3_r', 'glmed1_l', 'glmed1_r', 'glmed2_l', 'glmed2_r', 'glmed3_l', 'glmed3_r', 'glmin1_l', 'glmin1_r', 'glmin2_l', 'glmin2_r', 
-                       'glmin3_l', 'glmin3_r', 'iliacus_l', 'iliacus_r', 'psoas_l', 'psoas_r', 'piri_l', 'piri_r', 'tfl_l', 'tfl_r', 'sart_l', 'sart_r', 
-                       'recfem_l', 'recfem_r', 'edl_l', 'edl_r', 'ehl_l', 'ehl_r', 'fdl_l', 'fdl_r', 'fhl_l', 'fhl_r', 'tibant_l', 'tibant_r', 'tibpost_l', 
-                        'tibpost_r', 'perbrev_l', 'perbrev_r', 'perlong_l', 'perlong_r', 'gaslat_l', 'gaslat_r', 'gasmed_l', 'gasmed_r', 'soleus_l', 'soleus_r']
-
-
-    '''
-    ['addbrev_l', 'addbrev_r', 'addlong_l', 'addlong_r', 'addmagDist_l', 'addmagDist_r', 'addmagIsch_l', 
     '''
     tendon_names = ['addbrev_l_tendon', 'addbrev_r_tendon', 'addlong_l_tendon', 'addlong_r_tendon', 'addmagDist_l_tendon', 
                     'addmagDist_r_tendon', 'addmagIsch_l_tendon', 'addmagIsch_r_tendon', 'addmagMid_l_tendon', 'addmagMid_r_tendon', 
@@ -84,12 +61,10 @@ def storeData(env, model, steps, env_name, file_name):
                        'semimem_r', 'semiten_l', 'semiten_r', 'soleus_l', 'soleus_r', 'tfl_l', 'tfl_r', 'tibant_l', 'tibant_r', 'tibpost_l', 
                        'tibpost_r', 'vasint_l', 'vasint_r', 'vaslat_l', 'vaslat_r', 'vasmed_l', 'vasmed_r']
     '''
-    '''
     dataStore = {}
     dataStore['modelInfo'] = {}
     dataStore['modelInfo']['bodyNames'] =  body_names
     dataStore['modelInfo']['jointNames'] = joint_names
-    #dataStore['modelInfo']['tendonNames'] = tendon_names
     #dataStore['modelInfo']['tendonNames'] = tendon_names
     dataStore['modelInfo']['rewardWeights'] = env.rwd_keys_wt
     dataStore['modelInfo']['trainingSteps'] = 15000000
@@ -233,14 +208,12 @@ def storeData(env, model, steps, env_name, file_name):
         muscleMoment.append(env.sim.data.actuator_moment.copy())
         muscleVelocity.append(env.sim.data.actuator_velocity.copy())
 
+        #uncomment if need to produce videos
+        '''
         geom_1_indices = np.where(env.sim.model.geom_group == 1)
         env.sim.model.geom_rgba[geom_1_indices, 3] = 0
         frame_front = env.sim.renderer.render_offscreen(width=640, height=480,camera_id='front_view')
         frame_front = np.rot90(np.rot90(frame_front))
-        #frames_front.append(frame_front[::-1, :, :])
-        '''
-
-    tb_logdir = f"./standingBalance/temp_env_tensorboard/" + env_name + '/' + env_name + "_" + file_name + "_1"
         #frames_front.append(frame_front[::-1, :, :])
         '''
 
@@ -261,7 +234,6 @@ def storeData(env, model, steps, env_name, file_name):
 
     for joint in joint_names:
         qpos, qvel, qtau = [], [], []
-        hip_fle_l, hip_fle_r = [], []
         hip_fle_l, hip_fle_r = [], []
         for _ in range(steps):
             qpos.append(qpos_dict[joint][_])
@@ -284,7 +256,6 @@ def storeData(env, model, steps, env_name, file_name):
         dataStore['muscleInfo']['muscleActivation'][actuator] = muscleActivation
 
     dataStore['bodyInfo']['com'] = com
-    dataStore['bodyInfo']['com_v'] = com_v
     dataStore['bodyInfo']['height'] = com1[-1]
     dataStore['bodyInfo']['bos'] = bos
     dataStore['bodyInfo']['xpos'] = xpos
@@ -397,7 +368,7 @@ for ep in range(100):
     #os.makedirs(video_path, exist_ok=True)
 
     # make a local copy skvideo.io.vwrite(video_path + file_name + '_side' + '.mp4', np.asarray(frames_side), inputdict = {'-r':'100'}, outputdict={"-pix_fmt": "yuv420p"})
-    skvideo.io.vwrite(video_path +  policy_name + '_' + str(ep)  + '.mp4', np.asarray(frames_front), inputdict = {'-r':'100'}, outputdict={"-pix_fmt": "yuv420p"})
+    # skvideo.io.vwrite(video_path +  policy_name + '_' + str(ep)  + '.mp4', np.asarray(frames_front), inputdict = {'-r':'100'}, outputdict={"-pix_fmt": "yuv420p"})
 
     with open(pkl_path + policy_name + '_lp_' + str(ep) + '.pkl', 'wb') as fp:
         pickle.dump(data, fp)
